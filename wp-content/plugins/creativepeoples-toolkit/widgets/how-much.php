8995abd7a3cf3a3ff4f56e403c1c2325858df758
Nazmul <nazmuls4@yahoo.com>
2020-06-21 15:03:08 +0600

    why section update

--- wp-content/plugins/creativepeoples-toolkit/widgets/how-much.php
+++ wp-content/plugins/creativepeoples-toolkit/widgets/how-much.php
@@ -88,7 +88,7 @@ class how_much_Section_Widget extends \Elementor\Widget_Base {
 				'label' => __( 'Section Big Title', 'plugin-name' ),
 				'type' => \Elementor\Controls_Manager::TEXTAREA,
 				'label_block' => true,
-				'default' => 'Our Invesment Philosophy',
+				'default' => 'How much do you need to reach your financial goals?',
 			]
 		);
         
@@ -98,7 +98,7 @@ class how_much_Section_Widget extends \Elementor\Widget_Base {
 				'label' => __( 'Section Small Title', 'plugin-name' ),
 				'type' => \Elementor\Controls_Manager::TEXTAREA,
 				'label_block' => true,
-				'default' => 'ABOUT US',
+				'default' => 'MEASURING WHAT MATTERS',
 			]
 		); 
 
@@ -106,28 +106,19 @@ class how_much_Section_Widget extends \Elementor\Widget_Base {
 
 		 
 		$this->start_controls_section(
-			'Philosophy_left_side',
+			'section_btn',
 			[
-				'label' => __( 'Investment Philosophy Left Content', 'plugin-name' ),
+				'label' => __( 'Section button', 'plugin-name' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
-
-		$this->add_control(
-			'Philosophy_left_side_desctiotion',
-			[
-				'label' => __( 'Left side description', 'plugin-name' ),
-				'type' => \Elementor\Controls_Manager::TEXTAREA,
-				'label_block' => true,
-				'default' => 'We only recommend investments that are tailored to your unique financial goals. We don’t follow the herd, jump on bandwagons or invest in speculative businesses with large environmental risks and liabilities. Our investments are fundamentally sound, possess a strong margin of safety and have a track record of success.S',
-			]
-		);
+ 
 		$this->add_control(
 			'btn_label', [
 				'label' => __( 'Button label', 'plugin-domain' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => __( 'read more' , 'plugin-domain' ),
-				'show_label' => false,
+				'default' => __( 'SEE YOUR RESULTS' , 'plugin-domain' ),
+				'label_block' => true,
 			]
 		);
 		$this->add_control(
@@ -137,6 +128,7 @@ class how_much_Section_Widget extends \Elementor\Widget_Base {
 				'type' => \Elementor\Controls_Manager::URL,
 				'placeholder' => __( 'https://your-link.com', 'plugin-domain' ),
 				'show_external' => true,
+				'label_block' => true,
 				'default' => [
 					'url' => '',
 					'is_external' => true,
@@ -147,31 +139,64 @@ class how_much_Section_Widget extends \Elementor\Widget_Base {
 
 		 
 		$this->end_controls_section();
+ 
 
 
 		$this->start_controls_section(
-			'Philosophy_right_side',
+			'why_list_section',
 			[
-				'label' => __( 'Investment Philosophy Right Side Content', 'plugin-name' ),
+				'label' => __( 'Why list content', 'plugin-name' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
+
+		$repeater = new \Elementor\Repeater();
+
+		$repeater->add_control(
+			'why_status', [
+				'label' => __( 'Why status', 'plugin-domain' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => __( 'I’m' , 'plugin-domain' ),
+				'label_block' => true,
+			]
+		);	
+
+		$repeater->add_control(
+			'why_icon', [
+				'label' => __( 'Why icon', 'plugin-domain' ),
+				'type' => \Elementor\Controls_Manager::MEDIA, 
+				'label_block' => true,
+			]
+		);	
+
+		$repeater->add_control(
+			'why_count', [
+				'label' => __( 'Why count', 'plugin-domain' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => __( '40' , 'plugin-domain' ),
+				'label_block' => true,
+			]
+		);	
+		$repeater->add_control(
+			'why_desc', [
+				'label' => __( 'Why description', 'plugin-domain' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'default' => __( 'Years Old' , 'plugin-domain' ),
+				'label_block' => true,
+			]
+		);	
+
 		$this->add_control(
-			'Philosophy_right_side_big_image',
+			'list',
 			[
-				'label' => __( 'Right side big thumbnail', 'plugin-name' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'label_block' => true, 
+				'label' => __( 'WHY List', 'plugin-domain' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => $repeater->get_controls(), 
+				'title_field' => '{{{ why_status }}}',
 			]
 		);
-		$this->add_control(
-			'Philosophy_left_side_big_image',
-			[
-				'label' => __( 'Left side small thumbnail', 'plugin-name' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'label_block' => true, 
-			]
-		);
+ 
+
 		$this->end_controls_section();
 
 	}
@@ -189,22 +214,60 @@ class how_much_Section_Widget extends \Elementor\Widget_Base {
 		$settings = $this->get_settings_for_display();
 
 		$section_big_title = $this->get_settings('section_big_title');    
-		$section_small_title = $this->get_settings('section_small_title');     
-		$Philosophy_left_side_desctiotion = $this->get_settings('Philosophy_left_side_desctiotion');     
+		$section_small_title = $this->get_settings('section_small_title');   
 		$btn_label = $this->get_settings('btn_label');     
-		$btn_link = $this->get_settings('btn_link');     
-		$Philosophy_right_side_big_image = $this->get_settings('Philosophy_right_side_big_image');     
-		$Philosophy_left_side_big_image = $this->get_settings('Philosophy_left_side_big_image');
+		$btn_link = $this->get_settings('btn_link');  
+		$list = $this->get_settings('list');  
 
 		$targett = $settings['btn_link']['is_external'] ? ' target="_blank"' : '';
 		$nofolloww = $settings['btn_link']['nofollow'] ? ' rel="nofollow"' : '';    
 ?>
 	
 	<section class="how-much-area position-relative pt-120 pb-100">
-		<div class="container">
+		<div class="container-fluid">
 			<div class="row">
-				
+				<div class="col-lg-12">
+					<div class="section-title text-center">
+						<h1><?php echo $section_big_title; ?></h1>
+						<h3><?php echo $section_small_title; ?></h3>
+					</div>
+				</div>
 			</div>
+			<div class="row">
+
+
+				<?php foreach ($list as $key => $listitem): ?>
+					<div class="col-lg-3">
+						<div class="single-why-item">
+							<div class="single-item-title">
+								<h2><?php echo $listitem['why_status'] ;?></h2>
+							</div>
+							<div class="single-why-item-meta d-flex align-items-center">
+								<div class="single-why-item-icon">
+									<img src="<?php echo $listitem['why_icon']['url'] ;?>" alt="">
+								</div>
+								<div class="single-why-item-meta-right">
+									<h1><?php echo $listitem['why_count'] ;?></h1>
+									<p><?php echo $listitem['why_desc'] ;?></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endforeach ?> 
+			</div>
+
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="why-section-learnmore-area  text-center">
+						<div class="site-btn hero-btn">
+			  				<a  <?php echo $targett; ?> <?php echo $nofolloww ?> href="<?php echo $btn_link['url']; ?>" class="primary-btn filled-btn text-uppercase"><?php echo $btn_label ?></a>
+			  			</div>
+					</div>
+				</div>
+			</div>
+
+
+
 		</div>
 		 
     </section>
